295d6e4d2ff549da430d600e7e4a19317f854f36
han <ruukr8080@gmail.com>
2025-08-04 16:37:33 +0900

    refactor: 환경 설정 분리 및 OAuth2, DB 개선

--- src/main/java/com/app/server/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/app/server/infrastructure/config/SecurityConfig.java
@@ -51,31 +51,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/api/auth/**", "/signin", "/signup", "/signout", "/refresh-token" ).permitAll()
-                            .requestMatchers("/h2-console/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
-                            .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                            .anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/auth/**", "/signin", "/signup", "/signout", "/refresh-token").permitAll()
+                                .requestMatchers("/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
 // 필터중에 OAuth2LoginAuthenticationFilter 필더가 oauth2/code/** 요청을 처리하게 해야댐.
 // permitAll()로 하면 인증 필터 체인이 작동하기 전에 요청을 통과시켜버림. authenticated()로 "인증된 요청만 허용"로 해야됨.
 // OAuth2AuthorizationCodeGrantFilter가 redirect-uri로 들어오는 요청을 자동으로 가로채서 토큰 교환을 시도 함.
-
-            )
-            .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-            // OAuth2 로그인 설정
-            .oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfo -> userInfo
-                    .userService(customOAuth2UserService) // 커스텀 OAuth2 사용자 서비스
                 )
-                .successHandler(oAuth2SuccessHandler) // 로그인 성공 핸들러
-                .failureHandler(oAuth2FailureHandler) // 로그인 실패 핸들러
-            );
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                // OAuth2 로그인 설정
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService) // 커스텀 OAuth2 사용자 서비스
+                        )
+                        .successHandler(oAuth2SuccessHandler) // 로그인 성공 핸들러
+                        .failureHandler(oAuth2FailureHandler) // 로그인 실패 핸들러
+                );
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
